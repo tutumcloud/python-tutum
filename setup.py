688433a362ceb758445950be03f7cecf6a5b3364
Fernando <fermayo@gmail.com>
2014-04-04 13:19:32 -0400

    bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name = "python-tutum",
-    version = "0.6.3.4",
+    version = "0.6.3.5",
     packages = find_packages(),
     install_requires = ['requests>=2.2.1'],
     author = "Tutum, Inc.",
