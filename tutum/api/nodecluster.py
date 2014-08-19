6ac655fa342d72e8797889afd25566643497bb0d
tifayuki <tifayuki@gmail.com>
2014-08-19 18:56:06 -0400

    add deploy func to nodecluster

--- tutum/api/nodecluster.py
+++ tutum/api/nodecluster.py
@@ -5,3 +5,11 @@ class NodeCluster(RESTModel):
     """Represents a Tutum nodecluster object"""
 
     endpoint = "/nodecluster"
+
+    def deploy(self, tag=None):
+        """Deploy the nodeCluster.
+
+        :returns: bool -- whether or not the operation succeeded
+        :raises: TutumApiError
+        """
+        return self._perform_action("deploy")
