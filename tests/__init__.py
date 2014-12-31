1ec04960b501038d141f098f4b7d05a17de2920a
Anil Kulkarni <AnilRedshift@users.noreply.github.com>
2014-12-31 00:34:02 -0800

    fixing unittest.mock error

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,1 +1,18 @@
 __author__ = 'fermayo'
+
+try:
+    import configparser
+except ImportError:
+    import sys
+    sys.modules['configparser'] = __import__('ConfigParser')
+
+# Python 3.4.2 includes mock in-box, prefer that version
+# For other versions, patch it up to use the external mock library
+try:
+    from unittest import mock
+except ImportError:
+    import sys
+    sys.modules['unittest'] = __import__('unittest')
+    sys.modules['unittest.mock'] = __import__('mock')
+    setattr(sys.modules['unittest'], 'mock', __import__('mock'))
+
