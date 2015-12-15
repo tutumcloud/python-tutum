b921d54d5c4e490007c0e2b328eff8ee8916d102
tifayuki <tifayuki@gmail.com>
2015-12-15 15:58:39 +0100

    bump ver

--- tutum/__init__.py
+++ tutum/__init__.py
@@ -27,7 +27,7 @@ from tutum.api.utils import Utils
 from tutum.api.events import TutumEvents
 from tutum.api.nodeaz import AZ
 
-__version__ = '0.21.1'
+__version__ = '0.21.2'
 
 # Set auth properties: tutum_auth, basic_auth, and apikey
 
